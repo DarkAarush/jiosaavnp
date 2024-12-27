cb83426fc6d86cdef83d62fab11e3fc15fe60d9a
DarkAarush <128771887+DarkAarush@users.noreply.github.com>
2024-12-27 19:10:05 +0530

    Update stream.py

--- JioSavaan/utils/stream/stream.py
+++ JioSavaan/utils/stream/stream.py
@@ -10,7 +10,7 @@ from JioSavaan.core.call import Anony
 from JioSavaan.misc import db
 from JioSavaan.utils.database import add_active_video_chat, is_active_chat
 from JioSavaan.utils.exceptions import AssistantErr
-from JioSavaan.utils.inline import aq_markup, close_markup, stream_markup
+#from JioSavaan.utils.inline import aq_markup, close_markup, stream_markup
 from JioSavaan.utils.pastebin import AnonyBin
 from JioSavaan.utils.stream.queue import put_queue, put_queue_index
 from JioSavaan.utils.thumbnails import get_thumb
